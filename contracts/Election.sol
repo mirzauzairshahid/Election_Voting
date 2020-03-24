3ad0c9cfd7685b9dacc5abcfede52239169d172e
mirzauzairshahid <51515725+mirzauzairshahid@users.noreply.github.com>
2020-03-24 09:30:25 +0500

    Update Election.sol

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,24 +1,50 @@
-pragma solidity ^0.5.16;
+pragma solidity ^0.4.25;
 //pragma solidity ^0.6.1;
 contract Election {
-    // Modal a candidate
+    // Model a Candidate
     struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
-        //store candidate
-        //Fetch Candidate
-       mapping(uint => Candidate) public candidates;
-       // store candidate count;
-       uint public candidateCount;
-      constructor() public {
-            addCandidate("candidate 1");
-            addCandidate("candidate 2");
-       }
-       function addCandidate (string memory _name) private {
-        candidateCount ++;
-        candidates[candidateCount] = Candidate(candidateCount, _name, 0);
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    constructor () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender],"require that they haven't voted before");
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount,"require a valid candidate");
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
     }
 }
-/*var app = Election.deployed(); initial vote value is 1*/
